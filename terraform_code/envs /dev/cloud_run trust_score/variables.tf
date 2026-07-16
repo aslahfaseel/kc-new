52552b628690136878cf5a220271c3a863dfeec0
Aslah Faseel K <144783423+aslahfaseel@users.noreply.github.com>
2026-07-16 17:44:10 +0530

    Add variables for Cloud Run trust score
    Define variables for project configuration in Terraform.

---
+++ terraform_code/envs /dev/cloud_run trust_score/variables.tf
@@ -0,0 +1,25 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "location" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "terraform_sa" {
+  type = string
+}
+
+variable "image_tag" {
+  description = "The commit hash tag for the container image"
+  type        = string
+  # No default! We want it to fail if Jenkins doesn't pass the hash.
+}
+
+# Testing pipeline deployment trigger
