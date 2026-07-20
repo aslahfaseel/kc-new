1f2caa96bb9abbd26b8c10b09d10f3a76ac71ded
Aslah Faseel K <144783423+aslahfaseel@users.noreply.github.com>
2026-07-20 14:40:22 +0530

    Create variable.tf

---
+++ terraform_code/envs/dev/cloud_run/bulk_aspect_apply/variable.tf
@@ -0,0 +1,22 @@
+﻿variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "location" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "terraform_sa" {
+  type = string
+}
+
+variable "image_tag" {
+  description = "The commit hash tag for the container image"
+  type        = string
+}
